79655618aa53e804c08f593fcb9d50796817c640
jm-program <49599651+jm-program@users.noreply.github.com>
2020-09-20 14:08:48 +0300

    Согласование с условием задачи

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -14,6 +14,7 @@ public class Application {
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
+
         }
     }
 
